667fd2fcb54dd0563123681b869a525556d34e27
bmikolajczak <br.mikolajczak@gmail.com>
2023-03-09 01:31:13 +0100

    lint

--- App.tsx
+++ App.tsx
@@ -6,15 +6,7 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, ScrollView, StatusBar, StyleSheet} from 'react-native';
 
 import {Home} from './src/features/home/screens/homeScreen';
 
